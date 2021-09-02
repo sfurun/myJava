0dd319e56b4ac1fab883861d05f1c6c51ec69f4c
sfurun <sfurun@hotmail.com>
2021-09-02 13:31:29 +0300

    Data Type Commit

---
+++ DataType.java
@@ -0,0 +1,52 @@
+package gitProject;
+import java.util.*;
+
+public class DataType {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                try{
+                    byte y = (byte) x;
+                    if(y == x)
+                    {
+                        System.out.println("* byte");    
+                    }
+                }catch(Exception e){}
+                try{
+                    short y = (short) x;
+                    if(y == x)
+                    {
+                        System.out.println("* short");
+                    }
+                }catch(Exception e){}
+                try{
+                    int y = (int) x;
+                    if(y == x)
+                    {
+                        System.out.println("* int");
+                    }
+                }catch(Exception e){}
+                
+                
+                
+                System.out.println("* long");
+                //Complete the code
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+
+        }
+	}
+
+}
