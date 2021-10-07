a9f5923c570ab963909864bb6bc525a1f862e549
sfurun <sfurun@hotmail.com>
2021-10-07 09:31:08 +0300

    Commit edildi.

---
+++ MainBoks.java
@@ -0,0 +1,10 @@
+package gitProject;
+
+public class MainBoks {
+	
+	Fighter marc = new Fighter("Marc" , 15 , 100, 90, 40);
+    Fighter alex = new Fighter("Alex" , 10 , 95, 100, 40);
+    Ring r = new Ring(marc,alex , 90 , 100);
+    
+
+}
